84dfa17792f87bb4aab316348e6603d03c1c85c4
melzmatz <melendezdiana95@gmail.com>
2024-01-23 23:28:59 -0700

    added script to register page

---
+++ registration.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var createAccountBtn = document.querySelector('.login-btn');
+
+    createAccountBtn.addEventListener('click', function () {
+        alert('Thank you, Account created!');
+        
+    });
+});
