82f0c03d01728c0c29b7d4839c8e04160a29b6dd
Sebastian Acosta <mu9seb@gmail.com>
2024-01-23 23:56:08 -0700

    all buttons functional.

--- Java/java.js
+++ Java/java.js
@@ -2,24 +2,41 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btnList = document.getElementsByClassName("myBtn");
+
+// Get the <button> element that closes the modal
+var closeBtn = document.getElementById("closeBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementById("closeSpan");
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btnList[0].onclick = displayModal;
+btnList[1].onclick = displayModal;
+btnList[2].onclick = displayModal;
+btnList[3].onclick = displayModal;
+if (btnList.length > 4) {
+  btnList[4].onclick = displayModal;
+  btnList[5].onclick = displayModal;
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+
+// When the user clicks on <button> or span (x), close the modal
+closeBtn.onclick = closeModal;
+span.onclick = closeModal;
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    closeModal();
   }
 }
+
+function displayModal() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
